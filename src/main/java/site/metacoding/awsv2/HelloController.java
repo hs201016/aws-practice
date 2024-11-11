6263e5e2148f9261eea9c35a1f8afdc071de097e
류희수 <hs201016@gmail.com>
2024-11-11 11:25:19 +0900

    다시 커밋합니다요

--- src/main/java/site/metacoding/awsv2/HelloController.java
+++ src/main/java/site/metacoding/awsv2/HelloController.java
@@ -13,13 +13,13 @@ public class HelloController {
     @GetMapping("/aws/v2")
     public String hello(@RequestParam(defaultValue = "1") Integer number){
         if(number == 1){ // info 로그
-            log.info("/aws/v2 이 호출되었어요. info 로그 #####################################");
+            log.info("/aws/v2 이 2222. info 로그 #####################################");
         }else if(number == -1){ // error 로그
-            log.error("/aws/v2 이 호출되었어요. error 로그 #####################################");
+            log.error("/aws/v2 이 호출2222되었어요. error 로그 #####################################");
         }else if(number == 0){ // warn 로그
-            log.warn("/aws/v2 이 호출되었어요. warn 로그 #####################################");
+            log.warn("/aws/v2 이 호출되2222었어요. warn 로그 #####################################");
         }
         
-        return "<h1>aws22222 v2</h1>";
+        return "<h1>aws v2</h1>";
     }
 }
